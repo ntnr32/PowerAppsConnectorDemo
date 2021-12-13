2172c85ec60e6f8bc2b40fa92a6f43cb1e224e26
Nitin Res <nires@microsoft.com>
2021-12-14 01:15:18 +0530

    Add project files.

---
+++ CustomAPI/Configurations/OpenApiConfigurationOptions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAPI.Configurations
+{
+    internal class OpenApiConfigurationOptions : DefaultOpenApiConfigurationOptions
+    {
+        public override OpenApiInfo Info { get; set; } = new OpenApiInfo()
+        {
+            Version = "3.0.0",
+            Title = "Azure Function based Custom API Documentation",
+            Description = "A sample API that runs on Azure Functions using OpenAPI specification, running on the isolated worker environment.",
+            TermsOfService = new Uri("https://github.com/Azure/azure-functions-openapi-extension"),
+            Contact = new OpenApiContact()
+            {
+                Name = "Contoso",
+                Email = "azfunc-openapi@contoso.com",
+                Url = new Uri("https://github.com/Azure/azure-functions-openapi-extension/issues"),
+            },
+            License = new OpenApiLicense()
+            {
+                Name = "MIT",
+                Url = new Uri("http://opensource.org/licenses/MIT"),
+            }
+        };
+
+        public override OpenApiVersionType OpenApiVersion { get; set; } = OpenApiVersionType.V3;
+    }
+}
---
+++ CustomAPI/Configurations/OpenApiCustomUIOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAPI.Configurations
+{
+    public class OpenApiCustomUIOptions : DefaultOpenApiCustomUIOptions
+    {
+        public OpenApiCustomUIOptions(Assembly assembly) : base(assembly)
+        {
+        }
+
+        public override string CustomStylesheetPath => "dist.styles.css";
+
+        public override string CustomJavaScriptPath => "dist.script.js";
+    }
+}
